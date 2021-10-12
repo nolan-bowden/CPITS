cf49aaa0e96a5facc2ca12fd63986f8f0dd2ea87
Nolan Bowden <92326395+nolan-bowden@users.noreply.github.com>
2021-10-12 15:47:30 -0500

    Update app.py

--- python_app/app.py
+++ python_app/app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+import requests, csv
 
-app = Flask(__name__)
+def getWeather(cities):
+    weather = []
+    for c in cities:
+        apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + c + '&appid=' + str(apiKey)
+        data = requests.get(apiURL).json()
+        weather.append(data)
+    return weather
 
-@app.route('/')
-def index():
-    return "Hello World!"
+def writeToCSV(weather):
+    outputFile = open('test.csv', 'w', newline='')
+    csvWriter = csv.writer(outputFile)
+    csvWriter.writerow(['Name', 'Weather', 'Temp'])
+    for w in weather:
+        csvWriter.writerow([w['name'], w['weather'][0]['main'], w['main']['temp']])
 
-app.run('0.0.0.0',5000)
+
+apiKey = 'f4b4e38c6f0c8058fe74e738c4721056'
+cities = ['Dallas', 'Houston', 'Austin', 'San+Antonio', 'Fort+Worth']
+weather = getWeather(cities)
+writeToCSV(weather)
